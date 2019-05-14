5fc7c53d4ffa3dbb8d5ed22196bf31d764fe095d
Isaac Curtis <iicurtis@outlook.com>
2019-05-14 15:25:28 -0600

    change tapping_toggle

--- keyboards/redox/keymaps/icurtis/config.h
+++ keyboards/redox/keymaps/icurtis/config.h
@@ -36,4 +36,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_TERM 200
+#define TAPPING_TOGGLE 2
 
